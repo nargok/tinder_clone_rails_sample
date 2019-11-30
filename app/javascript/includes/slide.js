8f22822adc00fcae2c2a54cc7e4958e991ec82be
nargok <nargok@gmail.com>
2019-11-30 16:44:45 +0900

    slide show ui

---
+++ app/javascript/includes/slide.js
@@ -0,0 +1,28 @@
+$(function () {
+    var $activeSlide = $('#slides .slide:first-child');
+
+    $activeSlide.addClass("showing");
+
+    $('#decline').on("click", function () {
+        goToSlide('decline');
+    });
+
+    $('#approve').on("click", function () {
+        goToSlide('approve');
+    });
+
+    function goToSlide(action) {
+        $activeSlide.removeClass('showing');
+        $activeSlide = $activeSlide.next(".slide");
+
+        if (action == 'approve') {
+
+        } else {
+        }
+
+        $activeSlide.addClass('showing');
+    }
+});
+
+
+
